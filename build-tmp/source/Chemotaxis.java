de5c34693fd91b6f14f3119a7f55fcfc27eab631
gdare <gregory.dare27@gmail.com>
2014-09-22 11:17:27 -0700

    bacteria

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,7 +1,23 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
  Bacteria two;
  Bacteria [] colony = new Bacteria[5];
  int wait = 0;
- void setup(){
+ public void setup(){
  	size(300,300);
  	two = new Bacteria(150,100);
  	for(int i = 0; i < colony.length; i++){
@@ -9,7 +25,7 @@
 
  	}
  }   
- void draw(){
+ public void draw(){
  	background(0);
  	two.randomJump();
  	two.show(); 
@@ -26,7 +42,7 @@
     	xpos = x;
     	ypos = y;
     }
-    void randomWalk(){
+    public void randomWalk(){
     	fill(255,0,0);
         xpos = xpos + (int)(Math.random() * 11) - 5;
         ypos = ypos + (int)(Math.random() * 11) - 5;
@@ -35,7 +51,7 @@
         	ypos = 150;
         }
     }
-    void randomJump(){
+    public void randomJump(){
     	fill(0,255,0);
     	if(wait == 100){
     	    xpos = (int)(Math.random() * 300) + 1;
@@ -43,7 +59,16 @@
     	    wait = 0;
     	}
     }
-    void show(){
+    public void show(){
     	ellipse(xpos,ypos,15,15);
     }
  }    
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
